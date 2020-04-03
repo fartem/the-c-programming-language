b94d4d4e10c18c2996912847bcd6018f2dd02529
fartem <jaman.smlnsk@gmail.com>
2020-04-03 07:53:37 +0300

    2020-04-03 Update: Completed "Chapter 2" section

---
+++ Chapter_2/Exercise_2.10/lower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+ * Rewrite the function lower, which converts upper case letters to lower case,
+ * with a conditional expression instead of if-else.
+ */
+
+int lower(int c);
+
+int main()
+{
+    int c;
+    while ((c = getchar()) != EOF)
+    {
+        putchar(lower(c));
+    }
+    return 0;
+}
+
+int lower(int c)
+{
+    return c >= 'A' && c <= 'Z' ? c + 'a' - 'A' : c;
+}
---
+++ Chapter_2/Exercise_2.9/bitcount.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+ * In a two’s complement number system, x &= (x-1) deletes the rightmost 1-bit in x.
+ * Explain why. Use this observation to write a faster version of bitcount.
+ */
+
+int bitCount(unsigned x);
+
+int main()
+{
+    printf("%d\n", bitCount((unsigned) 5));
+    return 0;
+}
+
+int bitCount(unsigned x)
+{
+    int b;
+    for (b = 0; x != 0; x &= x - 1)
+    {
+        b++;
+    }
+    return b;
+}
