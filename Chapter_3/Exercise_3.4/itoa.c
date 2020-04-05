96496382bfad26945564311e52dabc6c5582231a
fartem <jaman.smlnsk@gmail.com>
2020-04-05 13:41:22 +0300

    2020-04-05 Update: Updated "Chapter 3" section

---
+++ Chapter_3/Exercise_3.4/itoa.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LENGTH 1000
+
+/*
+ * In a two’s complement number representation, our version of itoa does not handle
+ * the largest negative number, that is, the value of n equal to -(2wordsize-1).
+ * Explain why not. Modify it to print that value correctly,
+ * regardless of the machine on which it runs.
+ */
+
+void itoa(int n, char s[]);
+void reverse(char s[]);
+int abs(int x);
+
+int main()
+{
+    int number = -2147483648;
+    char str[MAX_LENGTH];
+
+    printf("Number: %d\n", number);
+    
+    itoa(number, str);
+    printf("Number as string: %s\n", str);
+    return 0;
+}
+
+void itoa(int n, char s[])
+{
+    int i = 0;
+    int sign = n;
+
+    do
+    {
+        s[i++] = abs(n % 10) + '0';
+    } while ((n /= 10) != 0);
+
+    if (sign < 0)
+    {
+        s[i++] = '-';
+    }
+    s[i] = '\0';
+    reverse(s);
+}
+
+int abs(int x)
+{
+    return (x > 0) ? x : -x;
+}
+
+void reverse(char s[])
+{
+    int c;
+    int i;
+    int j;
+
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
+    {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
