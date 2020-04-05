7ac276e58afbbd194e441ce41e08b770d73489bb
fartem <jaman.smlnsk@gmail.com>
2020-04-05 18:08:36 +0300

    2020-04-05 Update: Completed "Chapter 3" section

---
+++ Chapter_3/Exercise_3.5/itob.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LENGTH 1000
+
+/*
+ * Write the function itob(n,s,b) that converts the integer n into a base b character representation
+ * in the string s. In particular, itob(n,s,16) formats s as a hexadecimal integer in s.
+ */
+
+void itob(int n, char s[], int b);
+void reverse(char s[]);
+
+int main()
+{
+    int number = 101;
+    int base = 16;
+    char str[MAX_LENGTH];
+
+    itob(number, str, base);
+
+    printf("Result for %d with base %d = %s\n", number, base, str);
+    return 0;
+}
+
+void itob(int n, char s[], int b)
+{
+    int i = 0;
+    int j;
+    int sign;
+
+    if ((sign = n) < 0)
+    {
+        n = -n;
+    }
+    
+    do
+    {
+        j = n % b;
+        s[i++] = (j <= 9) ? j + '0' : j + 'a' - 10;
+    } while ((n /= b) > 0);
+
+    if (sign < 0)
+    {
+        s[i++] = '-';
+    }
+    s[i] = '\0';
+    reverse(s);
+}
+
+void reverse(char s[])
+{
+    int c;
+    int i;
+    int j;
+
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
+    {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
---
+++ Chapter_3/Exercise_3.6/itoa.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LENGTH 1000
+
+/*
+ * Write a version of itoa that accepts three arguments instead of two.
+ * The third argument is a minimum field width; the converted number must be padded
+ * with blanks on the left if necessary to make it wide enough.
+ */
+
+void itoa(int n, char s[], int w);
+void reverse(char s[]);
+
+int main()
+{
+    int number = 509;
+    int width = 10;
+    char str[MAX_LENGTH];
+
+    itoa(number, str, width);
+
+    printf("%s\n", str);
+    return 0;
+}
+
+void itoa(int n, char s[], int w)
+{
+    int i = 0;
+    int sign;
+
+    if ((sign = n) < 0)
+    {
+        n = -n;
+    }
+    
+    do
+    {
+        s[i++] = (n % 10) + '0';
+    } while ((n /= 10) > 0);
+    
+    if (sign < 0)
+    {
+        s[i++] = '-';
+    }
+    while (i < w)
+    {
+        s[i++] = ' ';
+    }
+    s[i] = '\0';
+    reverse(s);
+}
+
+void reverse(char s[])
+{
+    int c;
+    int i;
+    int j;
+
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
+    {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
