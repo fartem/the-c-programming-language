eb8361d6f036bb6bda616888cd9ee72a1e6c7342
fartem <jaman.smlnsk@gmail.com>
2020-06-27 17:32:30 +0300

    2020-06-27 Update: Updated Chapter 7

---
+++ Chapter_7/Exercise_7.3/minprintf.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/*
+ * Revise minprinf to handle more of the other facilities of printf.
+ */
+
+void minprintf(char *, ...);
+
+int main(void)
+{
+	char *a = "Some text";
+	minprintf("%s", a);
+	return 0;
+}
+
+void minprintf(char *fmt, ...)
+{
+	va_list ap;
+	char *p, *sval;
+	int ival;
+	double dval;
+
+	va_start(ap, fmt);
+	for (p = fmt; *p; p++)
+	{
+		if (*p != "%")
+		{
+			putchar(*p);
+			continue;
+		}
+		
+		switch (*++p)
+		{
+			case 'd':
+				ival = va_arg(ap, int);
+				printf("%d", ival);
+				break;
+			case 'f':
+				dval = va_arg(ap, double);
+				printf("%f", dval);
+				break;
+			case 's':
+				for (sval = va_arg(ap, char *); *sval; sval++)
+					putchar(*sval);
+				break;
+			default:
+				putchar(*p);
+				break;
+		}
+	}
+	va_end(ap);
+}
+
